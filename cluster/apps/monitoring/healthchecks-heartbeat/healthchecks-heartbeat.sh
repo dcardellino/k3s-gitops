aa0a7c723990a8889e66f17bc37ff2e4f29df1d8
Dominic Cardellino <dominic@cardellino.tech>
2022-08-07 14:20:48 +0200

    Fix cm and script

--- cluster/apps/monitoring/healthchecks-heartbeat/healthchecks-heartbeat.sh
+++ cluster/apps/monitoring/healthchecks-heartbeat/healthchecks-heartbeat.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ -z "${UPTIMEROBOT_HEARTBEAT_URL}" ]]; then
-    printf "%s - Yikes - Missing UPTIMEROBOT_HEARTBEAT_URL environment variable" "$(date -u)"
+if [[ -z "${UPTIMEROBOT_PING_URL}" ]]; then
+    printf "%s - Yikes - Missing UPTIMEROBOT_PING_URL environment variable" "$(date -u)"
     exit 0
 fi
 
-status_code=$(curl --connect-timeout 10 --max-time 30 -I -s -o /dev/null -w '%{http_code}' "${UPTIMEROBOT_HEARTBEAT_URL}")
+status_code=$(curl --connect-timeout 10 --max-time 30 -I -s -o /dev/null -w '%{http_code}' "${UPTIMEROBOT_PING_URL}")
 if [[ ! ${status_code} =~ ^[2|3][0-9]{2}$ ]]; then
     printf "%s - Yikes - Heartbeat request failed, http code: %s" "$(date -u)" "${status_code}"
     exit 0
